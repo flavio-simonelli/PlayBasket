aa9c644a0afc78ea319ef52d92f7a8684ae187ee
Samuele <tomassaccisamuele@gmail.com>
2024-02-09 13:14:52 +0100

    Aggiunto contrller grafico login

---
+++ progettoISPW/src/main/java/it/uniroma2/dicii/ispw/controller/controllerApplicativo/LoginControllerApplicativo.java
@@ -0,0 +1,2 @@
+package it.uniroma2.dicii.ispw.controller.controllerApplicativo;public class LoginControllerApplicativo {
+}
---
+++ progettoISPW/src/main/java/it/uniroma2/dicii/ispw/controller/controllerGrafico/interfaccia1/Login1ControllerGrafico.java
@@ -0,0 +1,2 @@
+package it.uniroma2.dicii.ispw.controller.controllerGrafico.interfaccia1;public class Login1ControllerGrafico {
+}
